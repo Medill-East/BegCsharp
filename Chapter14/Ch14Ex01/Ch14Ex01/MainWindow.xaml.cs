0f4d8d65bcfd114bd3ce4ffedf73997965d7044e
Medill-East <NEMailLHD@163.com>
2018-03-07 15:33:35 +0800

    BegCshape
    ch14ex01
    20180307 1533

--- Chapter14/Ch14Ex01/Ch14Ex01/MainWindow.xaml.cs
+++ Chapter14/Ch14Ex01/Ch14Ex01/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace Ch14Ex01
         {
             InitializeComponent();
         }
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, bubbling up");
+        }
+        private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, tunneling down");
+            e.Handled = true;
+        }
+        private void rotatedButton_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotatedButton handler, bubbling up");
+        }
+        private void rotatedButton_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotatedButton handler, tunneling down");
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, bubbling up");
+        }
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, tunneling down");
+        }
     }
 }
